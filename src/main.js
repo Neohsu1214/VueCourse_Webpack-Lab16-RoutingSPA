73a5657cb3353d3eaf720356f57c4c1953393250
Neo Hsu <neohsu@Neode-Mac-mini.local>
2020-04-10 20:59:35 +0800

    預設路徑會加上#以避免跟server中的真的目錄搞混！但也能加上mode:'history'使不加#

--- src/main.js
+++ src/main.js
@@ -9,7 +9,8 @@ Vue.use(VueRouter); // 記得要建立 routes.js 讓 VueRouter 載入routing規�
 // 初始化 vue-router 物件
 const router = new VueRouter(
   {
-    routes: routes // 也可以簡寫成 routes 就好
+    routes: routes, // 也可以簡寫成 routes 就好
+    mode: 'history' // 預設會在路徑前面加上#(hash tag)以避免跟 server 中的真的目錄搞混！但也能加上 mode: 'history' 跟 Vue說我不用加 hash tag
   }
 );
 
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,6 @@ import Home from "./components/Home";
 import User from "./components/user/User";
 
 export const routes = [
-    {path: '', component: Home},
+    {path: '', component: Home}, // 沒有給定 path 代表 Home 為 root route
     {path: '/user', component: User}
 ];
