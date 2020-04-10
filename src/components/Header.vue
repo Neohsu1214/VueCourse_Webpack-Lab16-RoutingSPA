2b9d5747c7b72044efd9f24cee0d761eb0581637
Neo Hsu <neohsu@Neode-Mac-mini.local>
2020-04-10 22:05:05 +0800

    加上 :param 就可以把動態資料帶給指定的 component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,14 +6,7 @@
                 <a class="nav-link active" href="https://google.com">Google</a>
             </li>
             <!-- 以上是bootstrap原有的範例，以下 比較一下 vue-router 的寫法 -->
-            <!-- 連結不再是<a href>而是<router-link to> -->
-            <li class="nav-item">
-                <router-link to="/">Home</router-link>
-            </li>
-            <li class="nav-item">
-                <router-link to="/user">User</router-link>
-            </li>
-            
+            <!-- 連結不再是<a href>而是<router-link to> -->        
             <!-- 使被點到的項目按鈕顏色不一樣，必須將li的宣告放進router-link讓他代為產生dom物件，並告知被點到的 class名稱，及用 exact初始化哪一項被點到 -->
             <router-link to="/" tag="li" active-class="active" exact><a>Home(RL)</a></router-link>
             <router-link to="/user" tag="li" active-class="active"><a>User(RL)</a></router-link>
--- src/routes.js
+++ src/routes.js
@@ -3,5 +3,5 @@ import User from "./components/user/User";
 
 export const routes = [
     {path: '', component: Home}, // 沒有給定 path 代表 Home 為 root route
-    {path: '/user', component: User}
+    {path: '/user/:id', component: User} // 加上 :param 就可以把動態資料帶給指定的 component
 ];
