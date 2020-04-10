cdc62a001bcd6442ff984fbb86ce3c889fb27fe7
Neo Hsu <neohsu@Neode-Mac-mini.local>
2020-04-10 20:35:19 +0800

    加入 routes 設定

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router' // 使用 VueRouter: 記得要先安裝套件 npm install vue-router --save/--save-dev
 import App from './App.vue'
+import { routes } from "./routes" // {} 中的 routes就是 routes.js 中的 const route
+
+// 跟 Vue說我要使用 VueRouter
+Vue.use(VueRouter); // 記得要建立 routes.js 讓 VueRouter 載入routing規則
+
+// 初始化 vue-router 物件
+const router = new VueRouter(
+  {
+    routes: routes // 也可以簡寫成 routes 就好
+  }
+);
 
 new Vue({
   el: '#app',
+  router: router, // 也可以簡寫成 router 就好
   render: h => h(App)
 })
