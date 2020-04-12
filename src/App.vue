a28e3bd247f6cdb4c7fb529311c2e2bddff3e136
Neo Hsu <neohsu@Neode-Mac-mini.local>
2020-04-12 14:09:32 +0800

    routing path 的切換也可以加上transition動畫

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,10 @@
                 <h1>Routing</h1>
                 <hr>
                 <app-header></app-header>
-                <!-- App.vue加上 router-view 來告訴 vue-router 將頁面內容放在這邊 -->
-                <router-view foo="123"></router-view>
+                <transition name="slide" mode="out-in"> <!-- routing path 的切換也可以加上動畫！ -->
+                    <!-- App.vue加上 router-view 來告訴 vue-router 將頁面內容放在這邊 -->
+                    <router-view foo="123"></router-view>                
+                </transition>
                 <!-- 可以在一個 route path中給定多個 components，讓不同component替換掉不同命名的 router-view -->
                 <router-view name="bar"></router-view>
                 <router-view name="bar2"></router-view> <!-- 有宣告但對應不到名字的 router-view 就不會顯示東東！ -->
@@ -26,4 +28,34 @@
 </script>
 
 <style>
+    /* 建立一個 transition 名為 slide 的classes */
+    .slide-enter {
+        /* transform: translateY(20px); 因為與slide-in 起始位置相同，可不用寫 */
+    }
+    .slide-enter-active {
+        animation: slide-in 1s ease-out forwards;
+    }
+    .slide-leave {
+        
+    }
+    .slide-leave-active {
+        animation: slide-out 1s ease-out forwards;
+    }
+
+    @keyframes slide-in {
+        from {
+            transform: translateY(20px);
+        }
+        to {
+            transform: translateY(0);
+        }
+    }
+    @keyframes slide-out {
+        from {
+            transform: translateY(0);
+        }
+        to {
+            transform: translateY(20px);
+        }
+    }
 </style>
